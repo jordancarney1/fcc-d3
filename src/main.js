269821afe6308730dbbb7e0d077486f3daa660ae
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-08-27 16:18:04 -0400

    Add css modules to project
    This commit adds css modules to project.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import * as d3 from 'd3'
+import styles from './main.css'
 
 class App extends Component {
   render () {
-    return <div>Hello, world!</div>
+    return <div className={styles.main}>Hello, world!</div>
   }
 }
 
