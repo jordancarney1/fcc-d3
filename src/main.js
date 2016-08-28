9dd4021df96e9c8d57685709f0c3818fde116b3c
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-08-28 19:25:37 -0400

    Add isomorphic fetch to project
    This commit adds isomorphic fetch to the project. This does not override
    the current fetch API in the window object but does allow for fetch to
    work across other browsers. For more information visit:
    https://github.com/matthew-andrews/isomorphic-fetch

--- src/main.js
+++ src/main.js
@@ -3,7 +3,26 @@ import { render } from 'react-dom'
 import * as d3 from 'd3'
 import styles from './main.css'
 
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+const URL = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+
 class App extends Component {
+
+  constructor () {
+    super()
+    this.state = {
+      data: {}
+    }
+  }
+
+  componentDidMount () {
+    fetch(URL)
+      .then(res => res.json())
+      .then(data => this.setState({data}))
+  }
+
   render () {
     return <div className={styles.main}>Hello, world!</div>
   }
