b328f61d1c6a107dab1922fd0b3472a254983041
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-09-10 20:58:34 -0400

    Add decorator support for project (#17)
    Added decorator support to the project for better connect syntax.
    Slightly modified stateless functional components with shorter syntax.

--- src/common/components/App.js
+++ src/common/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-const App = () => (
-  <div>
-    <ul>
-      <li><Link to='/bar-chart'>Bar Chart</Link></li>
-    </ul>
-  </div>
-)
-
-export default App
+export default function App() {
+  return (
+    <div>
+      <ul>
+        <li><Link to='/bar-chart'>Bar Chart</Link></li>
+      </ul>
+    </div>
+  )
+}
