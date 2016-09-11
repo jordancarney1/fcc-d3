ad3d1ff09df649129c7be341d2491a7eecc0037b
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-09-11 19:57:38 -0400

    Add needed Elm files

--- src/common/components/App.js
+++ src/common/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+import ElmComponent from './ElmComponent'
+
 export default function App() {
   return (
     <div>
+      <ElmComponent />
       <ul>
         <li><Link to='/bar-chart'>Bar Chart</Link></li>
       </ul>
