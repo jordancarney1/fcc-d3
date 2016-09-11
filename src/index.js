2196c859be693980e8806454aa47d4973093977e
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-09-10 20:31:41 -0400

    Update components and containers
    Update of the components and containers. I originally was going to have
    the App component fetch all data for the projects but I thought it might
    be better for each individual project/container do that itself. I also
    changed the components to be stateless functional components in, since
    they hold no state.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
 import reducers from '~/store/reducers'
-import Root from '~/containers/Root'
+import Root from '~/components/Root'
 
 const middleware = [ thunk ]
 
