c71a1089c1b6236ecd6fbca0f11ef290c635d120
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-09-12 21:55:28 -0400

    Add recompose to project
    Added recompose to project to handle state what is currently in state.
    Check out the cool knew thing I have. For more info on recompose visit:
    https://github.com/acdlite/recompose

--- src/common/components/App.js
+++ src/common/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-// import ElmComponent from './ElmComponent'
+import Dimensions from '../hoc/Dimensions'
 
 export default function App() {
   return (
     <div>
-      {/* One day...<ElmComponent /> */}
       <ul>
+        <Dimensions />
         <li><Link to='/bar-chart'>Bar Chart</Link></li>
       </ul>
     </div>
