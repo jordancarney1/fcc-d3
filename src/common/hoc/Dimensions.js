4ea81c06d493624d4944f5f241eda16f0b73b935
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-09-12 22:51:29 -0400

    A little clean-up
    Cleaned things up a little and refactored my code a bit. You know, to
    make it all elegant and shit.

--- src/common/hoc/Dimensions.js
+++ src/common/hoc/Dimensions.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Observable } from 'rx'
-import { mapPropsStream, setObservableConfig } from 'recompose'
+import { mapProps, mapPropsStream, setObservableConfig, compose } from 'recompose'
 import rxjs4config from 'recompose/rxjs4ObservableConfig'
 
 setObservableConfig(rxjs4config)
 
-const Enhance = mapPropsStream(props$ => {
+const getWindowDimensions = mapPropsStream(props$ => {
   const dimensions$ = Observable
     .fromEvent(window, 'resize')
     .map(e => ({ width: e.target.innerWidth, height: e.target.innerHeight }))
@@ -16,12 +16,23 @@ const Enhance = mapPropsStream(props$ => {
   }))
 })
 
-const Dimensions = Enhance(({ values }) =>
-  <div>
-    <h1>This is with the help of recompse! Yay!</h1>
-    <div>{values.width}</div>
-    <div>{values.height}</div>
-  </div>
-)
+const spreadProps = mapProps(({ values }) => ({
+  height: values.height,
+  width: values.width
+}))
 
-export default Dimensions
+const component = ({ width, height }) =>
+  <div style={{ width: '100vw', height: '50vh', backgroundColor: `rgb(${width}, ${height}, ${width - height})`}}>
+    <small>Make me smaller and the screen will start to turn yellow...ooooohhhhhh aaaaahhhhhhhh</small>
+    <h1>This is with the help of recompse! Yay!</h1>
+    <div>{width}</div>
+    <div>{height}</div>
+  </div>
+
+const dimensions = compose(
+  getWindowDimensions,
+  spreadProps
+)(component)
+
+
+export default dimensions
