3b6cac2ac11b856c238232ecab8ee29e671d559a
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-09-18 02:45:21 -0400

    Add ScatterPlot container (#32)
    Added ScatterPlot container that fetches data using a mix of React,
    Redux, Recompose, and RxJS. Cooooooooollllll ;)

--- src/common/components/Root.js
+++ src/common/components/Root.js
@@ -4,6 +4,7 @@ import { Router, Route } from 'react-router'
 
 import App from './App'
 import BarChart from '~/containers/BarChart'
+import ScatterPlot from '~/containers/ScatterPlot'
 
 export default function Root({ store, history }) {
   return (
@@ -12,6 +13,7 @@ export default function Root({ store, history }) {
         <Router history={history}>
           <Route path='/' component={App} />
           <Route path='/bar-chart' component={BarChart} />
+          <Route path='/scatter-plot' component={ScatterPlot} />
         </Router>
       </div>
     </Provider>
---
+++ src/common/containers/ScatterPlot.js
@@ -0,0 +1,44 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { mapProps } from 'recompose'
+
+import { fetchDataIfNeeded } from '~/store/actions'
+import * as selectors from '~/store/selectors'
+import { windowDimensions } from '~/hocs'
+
+const mapStateToProps = state => {
+  const getResource = selectors.getResource('scatter-plot')
+  return {
+    data: getResource(state)
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  fetchData() {
+    dispatch(fetchDataIfNeeded('scatter-plot', 'cyclist-data'))
+  }
+})
+
+@windowDimensions
+@connect(mapStateToProps, mapDispatchToProps)
+@mapProps(({ data, ...props}) => ({ ...data, ...props}))
+export default class ScatterPlot extends Component {
+
+  static propTypes = {
+    data: PropTypes.object
+  }
+
+  static defaultProps = {
+    data: {}
+  }
+
+  componentDidMount() {
+    const { fetchData } = this.props
+    fetchData()
+  }
+
+  render() {
+    const { data } = this.props
+    return <div>{JSON.stringify(data[0] || null)}</div>
+  }
+}
