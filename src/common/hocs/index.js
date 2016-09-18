2a38f91b9b2753b82ec7f1d1199bade718a8bf49
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-09-18 12:00:39 -0400

    Refactor fetchData hoc (#35)
    Refactored the fetchData hoc so that data can either be set in
    localStorage or sessionStorage.

--- src/common/hocs/index.js
+++ src/common/hocs/index.js
@@ -2,8 +2,8 @@ import { Observable } from 'rx'
 import { mapPropsStream, setObservableConfig } from 'recompose'
 import rxjs4config from 'recompose/rxjs4ObservableConfig'
 
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
+require('es6-promise').polyfill()
+require('isomorphic-fetch')
 
 setObservableConfig(rxjs4config)
 
@@ -18,23 +18,25 @@ export const windowDimensions = mapPropsStream(props$ => {
   }))
 })
 
+const fetchDataSet = resource =>
+  new Promise((resolve, reject) => {
+    if (!window[resource.storage].getItem(resource.name)) {
+      fetch(`https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/${resource.name}.json`)
+        .then(res => res.json())
+        .then(dataObject => {
+          const stringObject = JSON.stringify(dataObject)
+          window[resource.storage].setItem(resource.name, stringObject)
+          resolve(dataObject)
+        })
+        .catch(err => reject(err))
+    } else {
+      const dataObject = JSON.parse(window[resource.storage].getItem(resource.name))
+      resolve(dataObject)
+    }
+  })
+
 export const fetchData = resource => mapPropsStream(props$ => {
-  const promise$ = Observable.fromPromise(
-    () => new Promise((resolve, reject) => {
-      if (!window.localStorage.getItem(resource.name)) {
-        fetch(`https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/${resource.name}.json`)
-          .then(res => res.json())
-          .then(dataObject => {
-            window.localStorage.setItem(resource.name, JSON.stringify(dataObject))
-            resolve(dataObject)
-          })
-          .catch(err => reject(err))
-      } else {
-        const dataObject = JSON.parse(window.localStorage.getItem(resource.name))
-        resolve(dataObject)
-      }
-    })
-  )
+  const promise$ = Observable.fromPromise(fetchDataSet(resource))
   return props$.combineLatest(promise$, (props, data) => ({
     ...props,
     data
