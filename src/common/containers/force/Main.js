9b7208284b77b7b4bad0f393efc1b43d1e973a10
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-09-24 17:21:51 -0400

    Add force and globe folders
    Added the force directed graph and global graph folders to begin project
    setup. I may end up changing up the entire project to follow this setup
    but for now this is what we have.

---
+++ src/common/containers/force/Main.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import Force from './components/Force'
+
+export default function () {
+  return <Force />
+}
--- src/common/routes.js
+++ src/common/routes.js
@@ -7,12 +7,14 @@ import NoMatch from './components/NoMatch'
 import BarChart from './containers/BarChart'
 import ScatterPlot from './containers/ScatterPlot'
 import HeatMap from './containers/HeatMap'
+import Force from './containers/force/Main'
 
 export default (
-  <Route path="/" component={App}>
+  <Route path='/' component={App}>
     <IndexRoute component={Home}/>
-    <Route path="bar-chart" component={BarChart} />
-    <Route path="scatter-plot" component={ScatterPlot} />
+    <Route path='bar-chart' component={BarChart} />
+    <Route path='scatter-plot' component={ScatterPlot} />
+    <Route path='force' component={Force} />
     <Route path=':slug' component={HeatMap} />
     <Route path="*" component={NoMatch}/>
   </Route>
