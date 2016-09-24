2861f085f74cf32f0ebebc0fb0b5aef83d49283d
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-09-24 17:24:06 -0400

    Add force and globe folders (#41)
    Added the force directed graph and global graph folders to begin project
    setup. I may end up changing up the entire project to follow this setup
    but for now this is what we have.

--- src/common/components/App.js
+++ src/common/components/App.js
@@ -11,6 +11,7 @@ export default function App({ children }) {
         <li><Link to='/bar-chart'>Bar Chart</Link></li>
         <li><Link to='/scatter-plot'>Scatter Plot</Link></li>
         <li><Link to='/heat-map'>Heat Map</Link></li>
+        <li><Link to='/force'>Force Directed</Link></li>
       </ul>
       {children || <Home />}
     </div>
