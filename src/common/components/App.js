a0149c81b1e42ad912d2a7f818ecba2cb804e112
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-09-24 17:42:11 -0400

    Add globe route

--- src/common/components/App.js
+++ src/common/components/App.js
@@ -11,7 +11,8 @@ export default function App({ children }) {
         <li><Link to='/bar-chart'>Bar Chart</Link></li>
         <li><Link to='/scatter-plot'>Scatter Plot</Link></li>
         <li><Link to='/heat-map'>Heat Map</Link></li>
-        <li><Link to='/force'>Force Directed</Link></li>
+        <li><Link to='/force'>Force Directed Graph</Link></li>
+        <li><Link to='/globe'>Global Graphy</Link></li>
       </ul>
       {children || <Home />}
     </div>
