08e030ebf62a11a0837b3b06af8161e072b8264b
Jordan Carney <jordan@jordancarney.com>
2016-09-28 19:03:35 -0400

    Resolved Merge Conflicts, for real this time

--- src/main.js
+++ src/main.js
@@ -23,12 +23,8 @@ class App extends Component {
         barChart: 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json',
         scatterPlot: 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json',
         heatMap: 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json',
-<<<<<<< HEAD
-        forceGraph: 'https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json'
-=======
         forceGraph: 'https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json',
         meteorMap: 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json'
->>>>>>> b71054322cdffd9338a34200d96968e58578f955
       },
       margin: {
         top: 30,
@@ -70,10 +66,7 @@ class App extends Component {
         <ScatterPlot urls={this.state.urls} window={this.state.window} margin={this.state.margin} />
         <HeatMap urls={this.state.urls} window={this.state.window} margin={this.state.margin} />
         <ForceGraph url={this.state.urls.forceGraph} window={this.state.window} margin={this.state.margin} />
-<<<<<<< HEAD
-=======
         <MeteorMap url={this.state.urls.meteorMap} window={this.state.window} margin={this.state.margin} />
->>>>>>> b71054322cdffd9338a34200d96968e58578f955
       </div>
     )
   }
@@ -226,381 +219,7 @@ class BarChart extends Component {
           .on('mouseout', () => this.handleMouseOut())
 
       return card.toReact()
-<<<<<<< HEAD
-    }
 
-    return card.toReact()
-  }
-}
-
-//
-// SCATTER PLOT CARD
-//
-class ScatterPlot extends Component {
-
-  constructor() {
-    super()
-
-    this.state = {
-      data: false
-    }
-  }
-
-  handleHover(d) {
-    const theToolTip = document.getElementById('scatter-tool-tip')
-    theToolTip.innerHTML = '<strong>' + d.Name + '</strong> - ' + d.Nationality +
-                           '<br>Time: ' + d.Time +
-                           '<br>Year: ' + d.Year +
-                           '<br>' + d.Doping
-    theToolTip.style.opacity = 0.9
-    theToolTip.style.left = (d3.event.pageX + 4) + 'px'
-    theToolTip.style.top = (d3.event.pageY - 44) + 'px'
-  }
-
-  handleMouseOut() {
-    const theToolTip = document.getElementById('scatter-tool-tip')
-    theToolTip.style.opacity = 0
-  }
-
-  componentDidMount() {
-    fetch(this.props.urls.scatterPlot)
-      .then(res => res.json())
-      .then(data => this.setState(
-          Object.assign(this.state, {
-            data: data
-          })
-        )
-      )
-  }
-
-  render() {
-
-      // Set dimensions and margins of the page elements based on viewport dimensions
-      const margin = {
-        top: this.props.margin.top,
-        right: this.props.margin.right,
-        bottom: this.props.margin.bottom,
-        left: this.props.margin.left
-      }
-      const width = this.props.window.width - 2 * (margin.left + margin.right)
-      const height = this.props.window.height / 2 - (margin.top - margin.bottom)
-
-      //Define elements for graph
-      const card = ReactFauxDOM.createElement('div')
-      card.setAttribute('class', 'card')
-
-      const cardHeader = ReactFauxDOM.createElement('h2')
-      cardHeader.textContent = this.state.data ? 'Doping in Professional Bicycle Racing' : 'Loading...'
-
-      const footer = ReactFauxDOM.createElement('div')
-      footer.setAttribute('class', 'card-footer')
-
-      const cardFooter = ReactFauxDOM.createElement('p')
-      cardFooter.textContent = this.state.data ? '35 Fastest times up Alpe d\'Huez - Normalized to 13.8km distance' : ''
-
-      // Begin setting up DOM elements. **There is surely a better way to organize this...
-      card.appendChild(cardHeader)
-      footer.appendChild(cardFooter)
-
-    if (this.state.data) {
-
-      const graph = ReactFauxDOM.createElement('svg')
-      graph.setAttribute('width', width + margin.left + margin.right)
-      graph.setAttribute('height', height + margin.top + margin.bottom)
-
-      // Define the div for the tooltip
-      const toolTip = ReactFauxDOM.createElement('div')
-      toolTip.setAttribute('class', 'tooltip')
-      toolTip.setAttribute('id', 'scatter-tool-tip')
-      toolTip.style.setProperty('opacity', 0)
-
-      //Build DOM for graph structure. **There is surely a better way to organize this...
-      card.appendChild(toolTip)
-      card.appendChild(graph)
-      card.appendChild(footer)
-
-      // Get Data Ready
-      const dataSet = this.state.data
-
-      const formatTime = d3.timeFormat('%M:%S')
-      const formatMinutes = function (d) {
-        return formatTime( new Date( 2012, 0, 1, 0, 0, d ))
-      }
-
-      // Define scaling for graph
-      const x = d3.scaleLinear()
-        //.domain([d3.min(dataSet, d => d.Seconds), d3.max(dataSet, d => d.Seconds)])
-        .domain([0, (d3.max(dataSet, d => d.Seconds) - d3.min(dataSet, d => d.Seconds))])
-        .rangeRound([0, width])
-
-      const y = d3.scaleLinear()
-        .domain([1, d3.max(dataSet, d => d.Place)])
-        .range([height, 0])
-
-      const xAxis = d3.axisBottom(x).tickFormat(formatMinutes)
-
-      const yAxis = d3.axisLeft(y)
-
-      // Draw the Graph!
-      const bGraph = d3.select(graph)
-        .append('g')
-          .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-
-      // Draw the X axis
-      bGraph.append('g')
-          .attr('class', 'x-axis')
-          .attr('transform', 'translate(0,' + height + ')')
-          .call(xAxis)
-      // X Axes Label
-      bGraph.append('text')
-          .attr('class', 'x-axis-label')
-          .style('text-anchor', 'middle')
-          .style('font-size', '12px')
-          .attr('transform', 'translate(' + width / 2 + ',' + (height + margin.top) + ')')
-          .text('Minutes Behind Fastest Time')
-
-      // Draw the Y axis
-      bGraph.append('g')
-          .attr('class', 'y-axis')
-          .call(yAxis)
-      // Y Axes Label
-      bGraph.append('text')
-        .attr('class', 'y-axis-label')
-        .style('text-anchor', 'end')
-        .style('font-size', '12px')
-        .attr('transform', 'translate(15, 0)rotate(-90)')
-        .text('Ranking')
-
-      // Draw the data-points
-      bGraph.selectAll('dot')
-          .data(dataSet)
-        .enter()
-        .append('circle')
-          .attr('class', d => d.Doping ? 'circ dope' : 'circ')
-          .attr('r', 5)
-          .attr('cx', data => {
-              let secondsBehind = data.Seconds - d3.min(dataSet, d => d.Seconds)
-              return x(secondsBehind)
-            })
-          .attr('cy', d => y(d.Place))
-          .on('mouseover', d => this.handleHover(d))
-          .on('mouseout', () => this.handleMouseOut())
-
-      return card.toReact()
-    }
-
-    return card.toReact()
-  }
-}
-
-//
-// HEAT MAP CARD
-//
-
-class HeatMap extends Component {
-  constructor() {
-    super()
-
-    this.state = {
-      data: false
-    }
-  }
-
-  handleHover(d, theYear, theTemp, theMonth) {
-    const theToolTip = document.getElementById('tool-tip')
-    theToolTip.innerHTML = '<strong>' + theYear + '</strong>' + ' - ' + theMonth + '<br />' + 
-                           '<strong>' + theTemp + ' &deg;C</strong><br />' +
-                            d3.format('.3f')(d.variance) + ' &deg;C'
-    theToolTip.style.opacity = 0.9
-    theToolTip.style.left = (d3.event.pageX + 4) + 'px'
-    theToolTip.style.top = (d3.event.pageY - 44) + 'px'
-  }
-
-  handleMouseOut() {
-    const theToolTip = document.getElementById('tool-tip')
-    theToolTip.style.opacity = 0
-  }
-
-  componentDidMount() {
-    fetch(this.props.urls.heatMap)
-      .then(res => res.json())
-      .then(data => this.setState(
-          Object.assign(this.state, {
-            data: data
-          })
-        )
-      )
-  }
-
-  render() {
-    // Set dimensions and margins of the page elements based on viewport dimensions
-    const margin = {
-      top: this.props.margin.top,
-      right: this.props.margin.right,
-      bottom: this.props.margin.bottom + 45,
-      left: this.props.margin.left + 25
-    }
-    const width = this.props.window.width - 2 * (margin.left + margin.right)
-    const height = this.props.window.height / 2 - (margin.top - margin.bottom)
-
-
-    //Define elements for graph
-    const card = ReactFauxDOM.createElement('div')
-    card.setAttribute('class', 'card')
-
-    const cardHeader = ReactFauxDOM.createElement('h2')
-    cardHeader.textContent = this.state.data ? 'Monthly Global Land-Surface Temperature' : 'Loading...'
-
-    const cardSubHeader = ReactFauxDOM.createElement('div')
-    cardSubHeader.setAttribute('class', 'card-subheader')
-    cardSubHeader.textContent = this.state.data? '1753 - 2015' : ''
-    
-    const cardSubHeaderSpan = ReactFauxDOM.createElement('div')
-    cardSubHeaderSpan.setAttribute('class', 'card-subheader card-subheader-one')
-    cardSubHeaderSpan.textContent = this.state.data ? 'Temperatures are in Celsius and reported as anomalies relative to the Jan 1951-Dec 1980 average.' : ''
-
-    const cardSubHeaderSpanTwo = ReactFauxDOM.createElement('div')
-    cardSubHeaderSpanTwo.setAttribute('class', 'card-subheader card-subheader-two')
-    cardSubHeaderSpanTwo.textContent = this.state.data ? 'Estimated Jan 1951-Dec 1980 absolute temperature C: 8.66 +/- 0.07' : ''
-
-    // Begin setting up DOM elements. **There is surely a better way to organize this...
-    card.appendChild(cardHeader)
-    card.appendChild(cardSubHeader)
-    card.appendChild(cardSubHeaderSpan)
-    card.appendChild(cardSubHeaderSpanTwo)
-
-    if (this.state.data) {
-
-      const graph = ReactFauxDOM.createElement('svg')
-      graph.setAttribute('width', width + margin.left + margin.right)
-      graph.setAttribute('height', height + margin.top + margin.bottom)
-
-      // Define the div for the tooltip
-      const toolTip = ReactFauxDOM.createElement('div')
-      toolTip.setAttribute('class', 'tooltip')
-      toolTip.setAttribute('id', 'heatmap-tool-tip')
-      toolTip.style.setProperty('opacity', 0)
-
-      //Build DOM for graph structure. **There is surely a better way to organize this...
-      card.appendChild(toolTip)
-      card.appendChild(graph)
-
-      // Get Data Ready
-      const dataSet = this.state.data.monthlyVariance
-      const baseTemp = this.state.data.baseTemperature
-
-      // Years (X)
-      const years = dataSet.map( d => new Date(d.year, 0))
-      const formatYear = d3.timeFormat('%Y')
-      const minYear = d3.min(years)
-      const maxYear = d3.max(years)
-
-      // Months (Y)
-      const months = dataSet.map( d => new Date(0, d.month-1))
-      const formatMonth = d3.timeFormat('%B')
-      const minMonth = d3.min(months)
-      const maxMonth = d3.max(months)
-
-      // Temps (Color)
-      const temps = dataSet.map( d => (baseTemp + d.variance))
-      const colorRange = ['#2433FF', '#3C2EE3', '#542AC7', '#6D26AB', '#85228F', '#9D1D74', '#B61958', '#CE153C', '#E61120', '#FF0D05']
-      
-      // Bar Sizes
-      const barWidth = Math.ceil(width / (maxYear.getFullYear() - minYear.getFullYear()))
-      const barHeight = Math.ceil(height / (maxMonth.getMonth() - minMonth.getMonth()))
-
-      // Define scaling for graph
-      const x = d3.scaleTime()
-        .domain([minYear, maxYear])
-        .rangeRound([0, width])
-
-      const y = d3.scaleTime()
-        .domain([minMonth, maxMonth])
-        .rangeRound([height, 0])
-
-      const colorFill = d3.scaleQuantize()
-        .domain(d3.extent(temps))
-        .range(colorRange)
-
-      // Define the axes
-      const xAxis = d3.axisBottom(x).tickFormat(formatYear)
-
-      const yAxis = d3.axisLeft(y).tickFormat(formatMonth)
-
-      //Draw the Graph!
-      const heatMap = d3.select(graph)
-        .append('g')
-          .attr('class', 'graph')
-          .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-
-      // Draw the X axis
-      heatMap.append('g')
-          .attr('class', 'x-axis')
-          .attr('transform', 'translate(0,' + (height + (barHeight / 2)) + ')')
-          .call(xAxis)
-
-      // Draw the Y axis
-      heatMap.append('g')
-          .attr('class', 'y-axis')
-          .call(yAxis)
-
-      // Draw the data-points
-      heatMap.selectAll('rect')
-        .data(dataSet)
-        .enter()
-        .append('rect')
-          .attr('class', 'heat-bar')
-          .attr('x', d => x(new Date(d.year, 0)))
-          .attr('y', d => y(new Date(0, d.month-1)) - (barHeight / 2))
-          .attr('width', barWidth)
-          .attr('height', barHeight)
-          .attr('fill', d => colorFill( d.variance + baseTemp ))
-          .on('mouseover', (d) => {
-            let theYear = d.year
-            let theTemp = d3.format('.3f')(d.variance + baseTemp)
-            let theMonth = formatMonth(new Date(0, d.month - 1))
-            this.handleHover(d, theYear, theTemp, theMonth )
-          })
-          .on('mouseout', d => this.handleMouseOut())
-
-      // Legend for heat map
-      const legendRectSize = 20
-      const keys = colorRange.length
-      const legend = d3.select(graph)
-        .append('g')
-          .attr('class', 'legend')
-      
-      // Minimum Range
-      legend.append('text')
-          .attr('class', 'range-min')
-          .attr('x', (width - legendRectSize * keys ) - 40)
-          .attr('y', height + 79)
-          .text( d3.format('.3f')(d3.min(dataSet.map( d => d.variance))))
-
-      // Color Scale
-      legend.selectAll('rect')
-          .data(colorFill.range())
-          .enter()
-        .append('rect')
-          .attr('class', 'key')
-          .attr('x', (d, i) => {
-            return legendRectSize * i + ( width - legendRectSize * keys )
-          })
-          .attr('y', height + 70)
-          .attr('width', legendRectSize)
-          .attr('height', legendRectSize / 2)
-          .style('fill', d => d)
-          .style('stroke', '#fff')
-      // Maximum Range
-      legend.append('text')
-          .attr('class', 'range-max')
-          .attr('x', (width - legendRectSize ) + 27)
-          .attr('y', height + 79)
-          .text( d3.format('.3f')(d3.max(dataSet.map( d => d.variance))))
-
-      return card.toReact()
-=======
->>>>>>> b71054322cdffd9338a34200d96968e58578f955
     }
 
     return card.toReact()
@@ -609,8 +228,6 @@ class HeatMap extends Component {
 }
 
 //
-<<<<<<< HEAD
-=======
 // SCATTER PLOT CARD
 //
 class ScatterPlot extends Component {
@@ -984,7 +601,6 @@ class HeatMap extends Component {
 }
 
 //
->>>>>>> b71054322cdffd9338a34200d96968e58578f955
 // FORCE-DIRECTED GRAPH
 //
 class ForceGraph extends Component {
@@ -1017,11 +633,7 @@ class ForceGraph extends Component {
     svg.append('div')
       .attr('id', 'force-tool-tip')
 
-<<<<<<< HEAD
-    d3.json(this.props.url, function(error, graph) {
-=======
     d3.json(this.props.url, function (error, graph) {
->>>>>>> b71054322cdffd9338a34200d96968e58578f955
       if (error) throw error;
 
       const node = d3.select('.nodes')
@@ -1059,18 +671,6 @@ class ForceGraph extends Component {
 
       function ticked() {
         link
-<<<<<<< HEAD
-            .attr('x1', function(d) { return d.source.x })
-            .attr('y1', function(d) { return d.source.y })
-            .attr('x2', function(d) { return d.target.x })
-            .attr('y2', function(d) { return d.target.y })
-
-        node
-            .attr('cx', function(d) { return d.x = Math.max(radius, Math.min(width - radius, d.x)) })
-            .attr('cy', function(d) { return d.y = Math.max(radius, Math.min(height - radius, d.y)) })
-            .attr('style', function(d) { 
-              let styleString =  'left:' + Math.max(radius, Math.min(width - radius, d.x)) + 'px; ' +
-=======
             .attr('x1', function (d) { return d.source.x })
             .attr('y1', function (d) { return d.source.y })
             .attr('x2', function (d) { return d.target.x })
@@ -1081,7 +681,6 @@ class ForceGraph extends Component {
             .attr('cy', function (d) { return d.y = Math.max(radius, Math.min(height - radius, d.y)) })
             .attr('style', function (d) {
               let styleString = 'left:' + Math.max(radius, Math.min(width - radius, d.x)) + 'px; ' +
->>>>>>> b71054322cdffd9338a34200d96968e58578f955
                 'top:' + Math.max(radius, Math.min(height - radius, d.y)) + 'px;'
               return styleString
             })
@@ -1141,8 +740,6 @@ class ForceGraph extends Component {
     )
   }
 }
-<<<<<<< HEAD
-=======
 
 class MeteorMap extends Component {
   componentDidMount() {
@@ -1297,6 +894,5 @@ class MeteorMap extends Component {
     )
   }
 }
->>>>>>> b71054322cdffd9338a34200d96968e58578f955
 
 render(<App />, document.getElementById('root'))
